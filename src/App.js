00f012e2ee8abcf8c48c264aec230bb7ad19a28f
DylanNoally <dnoally@litesoft.fr>
2019-03-14 16:28:16 +0100

    App React consommant une API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
 
     state = {
-        persons: []
+        // Déclaration d'un tableau pour y stocker les "applications" que retourne l'API
+        lesApps: []
     }
 
+    // Header conseillé pour des requêtes API retournant du JSON
     headers = {
         'Access-Control-Allow-Origin': '*',
         'Accept': 'application/json',
@@ -16,17 +17,21 @@ class App extends Component {
     }
 
     componentDidMount() {
-        axios.get(`http://www.apitest.e-quotationmodelapp.com:59444/QM/Fichiers?ID_Soc=2`, this.headers)
+        // URL pour consommer l'API en y ajoutant le header précédemment déclarer
+        axios.get(`VOTRE_URL`, this.headers)
             .then(res => {
-              const persons = res.data;
-                this.setState({ persons });
+                // Déclaration d'une constante pour récupérer toutes les applications (tableau de sous-niveau) du JSON
+              const lesApps = res.data.lesApplications;
+              console.log(lesApps);
+                this.setState({ lesApps });
             })
     }
 
     render() {
         return (
+            /* Retourne le résultat sous forme de liste avec "uneApp" qui aura l'équivalent d'une clé pour un foreach(uneApp in lesApps) */
             <ul>
-                { this.state.persons.map(person => <li>{person.name}</li>)}
+                { this.state.lesApps.map(function (uneApp, i) { return <li key={i}>N°{uneApp.id} - {uneApp.libelle} - Type : {uneApp.type} - Catégorie : {uneApp.categorie} - Statut : {uneApp.statut}</li> })}
             </ul>
         )
     }
